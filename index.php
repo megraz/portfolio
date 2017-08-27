f87b5cda7868e3ff13e827da258768e5d3ece899
Egraz Marlene <marlene.egraz@gmail.com>
2017-08-27 12:06:50 +0200

    home page with gozilla

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:700" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+    <title>Marlène Egraz</title>
+
+    <style type="text/css">
+    .nav-down{
+      margin-top: 20px;
+      font-family: 'Comfortaa', cursive;
+      font-size: 18px;
+    }
+    .stylename {
+      position: absolute;
+      left: 70px;
+      top: 200px;
+      font-family: 'Lobster', cursive;
+      font-size: 100px;
+      color: #000000;
+      text-shadow: 2px 2px 2px black, -2px -2px 2px white;
+    }
+    .vuelyon {
+      width: 100%;
+      height: 100%;
+      opacity: 0.5;
+    }
+    .godzilla {
+      position: absolute;
+      right: 120px;
+      top: 80px;
+      -webkit-filter: drop-shadow(5px 5px 5px black);
+      filter: drop-shadow(5px 5px 5px black);
+    }
+
+    </style>
+</head>
+
+<body>
+<header class="nav-down">
+<ul class="nav nav-pills nav-justified">
+  <li class="nav-item">
+    <a class="btn btn-outline-success" href="#" style="border-radius:10px">Accueil</a>
+  </li>
+  <li class="nav-item">
+    <a class="btn btn-outline-success" href="#" style="border-radius:10px">Mes projets</a>
+  </li>
+  <li class="nav-item">
+    <a class="btn btn-outline-success" href="#" style="border-radius:10px">A propos de moi</a>
+  </li>
+  <li class="nav-item">
+    <a class="btn btn-outline-success" href="#" style="border-radius:10px">Contact</a>
+  </li>
+</ul>
+</header>
+<div class="fondlyon">
+<img class="vuelyon" src="vue_lyonwm2.jpg">
+<h1 class="stylename">Marlène Egraz</h1>
+</div>
+<div class="monster">
+<img class="godzilla" src="godzilla_illustr1c.png">
+</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+
+</html>

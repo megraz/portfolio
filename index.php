86a1a95c3399377d7494d0354093ee7d2165b5a2
Egraz Marlene <marlene.egraz@gmail.com>
2017-10-31 11:54:33 +0100

    change link tmtemplate

--- index.php
+++ index.php
@@ -50,9 +50,9 @@ Graphiste de formation, j'ai un intérêt particulier pour le front-end.</p>
 </div>
 <section class="captureprojets">
   <div class="text-center">
-  
+
   <section id="template">
-    <a href="https://www.simplonlyon.fr/promo3/megraz/app">
+    <a href="http://www.simplonlyon.fr:3003/">
     <img src="screenshot-tptemplate.png" class="rounded mx-auto d-block" alt="screencap">
     </a>
     <p>Projet de groupe pour l'apprentissage de Node.js</p>
